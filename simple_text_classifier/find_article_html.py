c51090fd89293c2d63e899ec2b2ff3d4d0d51ae9
Sid <sid@e-share.us>
2019-07-26 10:51:49 -0400

    Refactored, working, still chokes on quotes

--- simple_text_classifier/find_article_html.py
+++ simple_text_classifier/find_article_html.py
@@ -12,6 +12,7 @@ import argparse
 import operator
 
 from bs4 import BeautifulSoup
+from __builtin__ import False
 
 #############################################    
 
@@ -20,6 +21,7 @@ def main(argv):
     script_name = "find_article_html.py"
 
     parser = argparse.ArgumentParser(description="Find the content bearing portion of an HTML page")
+    parser.add_argument('-d', '--debug', action="store_true", help="Show state machine for debugging")    
     parser.add_argument('filespec', help="path to the html file to extract")
     args = parser.parse_args()
         
@@ -64,111 +66,165 @@ def main(argv):
     section_links = 0
     section_link_density = 0.0
     emit = True
-    in_link = False
+    tag_done = False
 
     # state machine:
+    #################################
+    
     for ch in html:   
-        # print "\ttag:", tag, "\t:is_tag:", is_tag, "\tis_close_tag:", is_close_tag, "\tlink:", is_link, "\temit:", emit, "\t:", ch
+
         # ignore new lines     
         if ch in ignore_chars:
             continue
-        # ignore spaces at beginning of section
-        if ch == ' ':
-            if dict_article[section]['text'] == "":
-                continue
+        # ignore spaces at beginning of section (when not in_tag)
+        if not is_tag:
+            if ch == ' ':
+                if dict_article[section]['text'] == "":
+                    continue
         # end if
         # ignore more than 1 space
         if ch == ' ':
             if last == ' ':
-                last = ch
                 continue
         # end if
+        if args.debug:
+            print "is_tag:", is_tag, "close:", is_close_tag, "done:", tag_done, "link:", is_link, \
+                "emit:", emit, "last:", last, "tag:", tag, "\t:", ch
+            
+#         if ch in [ '"', "'" ]:
+#             in_quote = not in_quote
+#         if in_quote:
+#             continue
+
+        ################################# START TAG <
+        
         if ch == '<':
-            if last in ['"', "'"]:
-                # quoted, ignore 
-                continue
             # start tag
             is_tag = True
             is_close_tag = False
+            tag_done = False
+            emit = False
+            is_link = False
             tag = ""
             continue
         # end if
+
+        ################################# END TAG >
+
         if ch == '>':
             # end tag
             is_tag = False
+            tag_done = True
+            if last == "/":
+                is_close_tag = True
             if is_close_tag:
-                for t in remove_tags:
-                    if tag.startswith(t):
-                        # finished with a remove tag
-                        emit = True
+                # close tag
+                emit = True
                 if tag == 'a':
-                    in_link = False
-                for t in chunk_tags:
-                    if tag.startswith(t):
-                        # calculate density for this section
-                        dict_article[section]['tag'] = tag
-                        dict_article[section]['len'] = section_len
-                        dict_article[section]['links'] = section_links
-                        if section_len > 0:
-                            dict_article[section]['density'] = float( float(section_links) / float(section_len) )
-                            if dict_article[section]['density'] == 0:
-                                dict_rank[section] = float(dict_article[section]['len'])
-                            else:
-                                dict_rank[section] = float( float(dict_article[section]['density']) * float(dict_article[section]['len']))
-                            if dict_article[section]['tag'] == 'title':
-                                dict_rank[section] = dict_rank[section] * 1000
-                       # initialize next section
-                        section = section + 1
-                        dict_article[section] = {}
-                        dict_article[section]['text'] = ""
-                        dict_article[section]['tag'] = tag
-                        dict_article[section]['len'] = 0
-                        dict_article[section]['links'] = 0
-                        dict_article[section]['density'] = 0.0
-                        dict_rank[section] = 0.0
-                        section_len = 0
-                        section_links = 0
-                        section_link_density = 0.0
-                    # end if
-                # end for
+                    is_link = False
             else:
-                # starting tag
-                # note we don't store spaces in tags!!
-                if tag.lower().startswith('a'):
-                    in_link = True
+                # open tag
+                emit = True
+                if tag == 'a':
+                    is_link = True
                 for t in remove_tags:
                     if tag.startswith(t):
-                        # finished with a tag we want to remove
                         emit = False
                 # end for
+            # end if is_close_tag
+                
+            for t in chunk_tags:
+                if tag.startswith(t):
+                    # calculate density for this section
+                    dict_article[section]['tag'] = tag
+                    dict_article[section]['len'] = section_len
+                    dict_article[section]['links'] = section_links
+                    if section_len > 0:
+                        dict_article[section]['density'] = float( float(section_links) / float(section_len) )
+                        if dict_article[section]['density'] < 0.1:
+                            dict_rank[section] = float(dict_article[section]['len'])
+                        else:
+                            dict_rank[section] = float( float(dict_article[section]['density']) * float(dict_article[section]['len']))
+                        if dict_article[section]['tag'] == 'title':
+                            dict_rank[section] = dict_rank[section] * 1000
+                   # initialize next section
+                    section = section + 1
+                    dict_article[section] = {}
+                    dict_article[section]['text'] = ""
+                    dict_article[section]['tag'] = tag
+                    dict_article[section]['len'] = 0
+                    dict_article[section]['links'] = 0
+                    dict_article[section]['density'] = 0.0
+                    dict_rank[section] = 0.0
+                    section_len = 0
+                    section_links = 0
+                    section_link_density = 0.0
+                # end if
+            # end for
             continue
+            
         # end if
+
+        ################################# IN TAG           
+        
         if is_tag:
             # in tag
             # note we will never get ignore chars or spaces!
+            # new tag with /, means close tag
             if ch == '/':
-                is_close_tag = True
+                if tag == "":
+                    is_close_tag = True
             else:
-                tag = tag + ch
+                if not tag_done:
+                    if ch == " ":
+                        tag_done = True
+                        last = ch
+                        continue
+                    tag = tag + ch
+                    last = ch
+                else:
+                    continue
+
+        ################################# OUTSIDE TAG         
+        
         else:
             if emit:
                 # accumulate in appropriate section
-                section_len = section_len + 1
-                if in_link:
-                    section_links = section_links + 1
+                if ch.isalnum():
+                    section_len = section_len + 1
+                    if is_link:
+                        section_links = section_links + 1
+                if dict_article[section]['text'] == "":
+                    if ch == " ":
+                        last = ch
+                        continue
+                if ch == " ":
+                    if last == " ":
+                        continue
+                    if dict_article[section]['text'][-1:] == " ":
+                        last = ch
+                        continue
                 dict_article[section]['text'] = dict_article[section]['text'] + ch
+                # end if
+                last = ch
             # end if
-            last = ch
-        # end if
-    # end for
-                
+        # end for
+    
+    # state machine end
+    #################################    
+    
+    if args.debug:          
+        for key in dict_article.keys():
+            print dict_article[key]
+        sys.exit()
+    
     # calculate density for last section if necessary
     dict_article[section]['tag'] = tag
     dict_article[section]['len'] = section_len
     dict_article[section]['links'] = section_links
     if section_len > 0:
         dict_article[section]['density'] = float( float(section_links) / float(section_len) )
-        if dict_article[section]['density'] == 0:
+        if dict_article[section]['density'] < 0.1:
             dict_rank[section] = float(dict_article[section]['len'])
         else:
             dict_rank[section] = float( float(dict_article[section]['density']) * float(dict_article[section]['len']))
